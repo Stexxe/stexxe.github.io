f9bc064fe63500554db22d3bb76dc16cc7279f6d
Aleksei Tirman <aleksei.tirman@gmail.com>
2024-03-13 13:55:59 +0200

    Add Puzzle test

---
+++ tina/script.js
@@ -0,0 +1,31 @@
+document.getElementById('test').textContent = document.body.clientWidth.toString();
+
+const img = new Image();
+img.src = 'us.png';
+img.onload = () => {
+  const puzzle = new headbreaker.Canvas('puzzle', {
+    width: 430,
+    height: 650,
+    image: img,
+    pieceSize: 100,
+    proximity: 20,
+    borderFill: 10,
+    lineSoftness: 0.18,
+    preventOffstageDrag: true,
+    strokeWidth: 1
+  });
+
+  puzzle.adjustImagesToPuzzleHeight()
+  puzzle.autogenerate({
+    horizontalPiecesCount: 4, // TODO: tweak values
+    verticalPiecesCount: 4
+  });
+  // puzzle.shuffle(0.7);
+  puzzle.attachSolvedValidator()
+
+  puzzle.draw();
+
+  puzzle.onConnect((_piece, figure, _target) => {
+    console.log(_target.puzzle.isValid())
+  })
+}
