7dde1d0b8d86192e03c8c8d6cf45bdfe04a42f54
Aleksei Tirman <aleksei.tirman@gmail.com>
2024-03-14 13:56:29 +0200

    Test puzzle

--- anniversary/script.js
+++ anniversary/script.js
@@ -1,29 +1,63 @@
-// const img = new Image();
-// img.src = 'us.png';
-// img.onload = () => {
-//   const puzzle = new headbreaker.Canvas('puzzle', {
-//     width: 345,
-//     height: 650,
-//     image: img,
-//     pieceSize: 100,
-//     proximity: 20,
-//     borderFill: 10,
-//     lineSoftness: 0.18,
-//     preventOffstageDrag: true,
-//     strokeWidth: 1
-//   });
-//
-//   puzzle.adjustImagesToPuzzleHeight()
-//   puzzle.autogenerate({
-//     horizontalPiecesCount: 4,
-//     verticalPiecesCount: 4
-//   });
-//   // puzzle.shuffle(0.7);
-//   puzzle.attachSolvedValidator()
-//
-//   puzzle.draw();
-//
-//   puzzle.onConnect((_piece, figure, _target) => {
-//     console.log(_target.puzzle.isValid())
-//   })
-// }
+const app = document.getElementById('app')
+
+function clearPage() {
+  app.innerHTML = '';
+}
+
+function renderStart() {
+  const template = document.getElementById('start-page');
+  const clone = template.content.cloneNode(true);
+  app.appendChild(clone)
+
+  document.getElementById('play').addEventListener('click', () => {
+    clearPage();
+    renderPuzzle();
+  });
+}
+
+function renderPuzzle() {
+  const template = document.getElementById('puzzle-page');
+  const clone = template.content.cloneNode(true);
+  app.appendChild(clone)
+
+  initPuzzle();
+}
+
+renderStart();
+
+
+
+
+
+function initPuzzle() {
+  const img = new Image();
+  img.src = 'assets/us.png';
+  img.onload = () => {
+    const puzzle = new headbreaker.Canvas('puzzle', {
+      width: 345,
+      height: 650,
+      image: img,
+      pieceSize: 100,
+      proximity: 20,
+      borderFill: 10,
+      lineSoftness: 0.18,
+      preventOffstageDrag: true,
+      strokeWidth: 1
+    });
+
+    puzzle.adjustImagesToPuzzleHeight()
+    puzzle.autogenerate({
+      horizontalPiecesCount: 4,
+      verticalPiecesCount: 4
+    });
+    puzzle.shuffle(0.7);
+    puzzle.attachSolvedValidator()
+
+    puzzle.draw();
+
+    puzzle.onConnect((_piece, figure, _target) => {
+      console.log(_target.puzzle.isValid())
+    })
+  }
+}
+
